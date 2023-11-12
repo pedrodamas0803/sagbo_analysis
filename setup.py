ef20ba980a12dd2440c8a99a3d4ffbfc98c4c444
Pedro D. Resende <pedro.damas-resende@esrf.fr>
2023-11-12 10:04:25 +0100

    dvc setup

--- setup.py
+++ setup.py
@@ -3,12 +3,11 @@ import sys
 
 import setuptools
 
-current_folder = pathlib.Path(__file__).parent
 
 setuptools.setup(
-    name='sagbo_analysis',
-    version='0.0.1',
-    author='Pedro D. Resende',
-    author_email='pedro.damas-resende@esrf.fr',
-    description='wrapper to facilitate my life analysing sagbo in-situ tests'
+    name="sagbo_analysis",
+    version="0.0.1",
+    author="Pedro D. Resende",
+    author_email="pedro.damas-resende@esrf.fr",
+    description="wrapper to facilitate my life analysing sagbo in-situ tests",
 )
