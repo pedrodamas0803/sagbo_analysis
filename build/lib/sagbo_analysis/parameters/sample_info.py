c39af7ee6d4dc2673a392da7cd4ace1c026351ed
Pedro D. Resende <pedro.damas-resende@esrf.fr>
2023-11-13 12:00:51 +0100

    added dvc capabilities

--- build/lib/sagbo_analysis/parameters/sample_info.py
+++ build/lib/sagbo_analysis/parameters/sample_info.py
@@ -6,10 +6,10 @@ from dataclasses import dataclass
 @dataclass
 class SampleInfo:
 
-    '''
+    """
     Class that holds information about a sample in a SAGBO experiment for processing
 
-    sample_dir: str - directory where the data for the referred sample is stored, 
+    sample_dir: str - directory where the data for the referred sample is stored,
                       e.g.: '/data/visitor/proposal/beamline/session/sample_name'
 
     base_name: str - text appended to the sample name to save the sequence of datasets,
@@ -21,7 +21,7 @@ class SampleInfo:
 
     pca_flat_file: str - path to the file containing the PCA decomposition of the flat field images for the experiment,
                         ideally inside the 'processing_dir'
-    '''
+    """
 
     # acquisition information
     sample_dir: str
@@ -29,90 +29,86 @@ class SampleInfo:
     darks_path: str
     processing_dir: str
     flats_path: str
-    overwrite: bool = True 
+    overwrite: bool = True
 
-    flats_entry: str = '/1.1/measurement/marana'
-    projs_entry: str = '/1.1/measurement/marana'
-    darks_entry: str = '/1.1/measurement/marana'
-    angles_entry: str = '/1.1/measurement/diffrz'
-    load_entry: str = '/1.2/measurement/stress_adc_input'
-    distance_entry: str = '/1.1/instrument/positioners/nfdtx'
+    flats_entry: str = "/1.1/measurement/marana"
+    projs_entry: str = "/1.1/measurement/marana"
+    darks_entry: str = "/1.1/measurement/marana"
+    angles_entry: str = "/1.1/measurement/diffrz"
+    load_entry: str = "/1.2/measurement/stress_adc_input"
+    distance_entry: str = "/1.1/instrument/positioners/nfdtx"
 
-    energy:float = 65.35
+    energy: float = 65.35
     pixel_size_m: float = 0.63e-6
 
     dering: bool = False
-    backend:str = 'ASTRA'
-
+    backend: str = "ASTRA"
 
     @property
     def exp_name(self):
-        return self.sample_dir.split('/')[3]
+        return self.sample_dir.split("/")[3]
 
     @property
     def sample_name(self):
-        return self.sample_dir.split('/')[-2]
+        return self.sample_dir.split("/")[-2]
 
     @property
     def pca_flat_file(self):
-        return os.path.join(self.processing_dir, 'PCA_flats.h5')
+        return os.path.join(self.processing_dir, "PCA_flats.h5")
 
     @property
     def config_file(self):
-        return os.path.join(self.processing_dir, 'config.ini')
+        return os.path.join(self.processing_dir, "config.ini")
 
     @property
     def datasets(self):
         datasets = []
         for dataset in os.listdir(self.sample_dir):
             if self.sample_name in dataset and self.base_name in dataset:
-                datasets.append(f'{self.sample_dir}{dataset}/{dataset}.h5')
+                datasets.append(f"{self.sample_dir}{dataset}/{dataset}.h5")
         datasets.sort()
-        return {f'path_{ii+1}': dataset for ii, dataset in enumerate(datasets)}
+        return {f"path_{ii+1}": dataset for ii, dataset in enumerate(datasets)}
 
     def _generate_config(self):
-        ''' Generates the configuration file for the data processing'''
+        """Generates the configuration file for the data processing"""
 
         config = configparser.ConfigParser()
 
-        config['DIRECTORIES'] = {
-            'Acquisition_dir': self.sample_dir,
-            'Base_name': self.base_name,
-            'Darks_path': self.darks_path,
-            'Flats_path': self.flats_path,
-            'Processing_dir': self.processing_dir,
-            'PCA_flat_file': self.pca_flat_file,
-            'Experiment_name': self.exp_name,
-            'Sample_name': self.sample_name,
-            'Config_file': self.config_file
+        config["DIRECTORIES"] = {
+            "Acquisition_dir": self.sample_dir,
+            "Base_name": self.base_name,
+            "Darks_path": self.darks_path,
+            "Flats_path": self.flats_path,
+            "Processing_dir": self.processing_dir,
+            "PCA_flat_file": self.pca_flat_file,
+            "Experiment_name": self.exp_name,
+            "Sample_name": self.sample_name,
+            "Config_file": self.config_file,
         }
 
-        config['DATASETS'] = self.datasets
+        config["DATASETS"] = self.datasets
 
-        config['FLAGS'] = {
-            'dering' : self.dering,
-            'backend' : self.backend,
-            'overwrite' : self.overwrite
+        config["FLAGS"] = {
+            "dering": self.dering,
+            "backend": self.backend,
+            "overwrite": self.overwrite,
         }
 
-        config['ENTRIES'] = {
-            'Projections': self.projs_entry,
-            'Flats': self.flats_entry,
-            'Darks': self.darks_entry,
-            'Angles': self.angles_entry,
-            'Load': self.load_entry,
-            'Distance' : self.distance_entry,
+        config["ENTRIES"] = {
+            "Projections": self.projs_entry,
+            "Flats": self.flats_entry,
+            "Darks": self.darks_entry,
+            "Angles": self.angles_entry,
+            "Load": self.load_entry,
+            "Distance": self.distance_entry,
         }
 
-        config['PHASE'] = {
-            'Energy' : self.energy,
-            'Pixel_size_m': self.pixel_size_m
-        }
+        config["PHASE"] = {"Energy": self.energy, "Pixel_size_m": self.pixel_size_m}
 
         return config
 
     def write_config(self):
-        ''' Writes the configuration file at the given data processing directory.'''
+        """Writes the configuration file at the given data processing directory."""
 
         config = self._generate_config()
         if not os.path.exists(self.processing_dir):
@@ -120,21 +116,18 @@ class SampleInfo:
 
         self._create_processing_dirs()
 
-        with open(self.config_file, 'w') as configfile:
-
+        with open(self.config_file, "w") as configfile:
             config.write(configfile)
 
     def _create_processing_dirs(self):
-        ''' Creates the folders for each acquisition after checking if it exists or not. '''
+        """Creates the folders for each acquisition after checking if it exists or not."""
 
         for dataset in self.datasets.values():
-            dataset_name = os.path.splitext(dataset)[0].split('/')[-1]
+            dataset_name = os.path.splitext(dataset)[0].split("/")[-1]
             print(dataset_name)
 
             dataset_proc_dir = os.path.join(self.processing_dir, dataset_name)
 
             if not os.path.exists(dataset_proc_dir):
                 os.mkdir(dataset_proc_dir)
-                print(f'Created {dataset_name} processing folder.')
-
-
+                print(f"Created {dataset_name} processing folder.")
