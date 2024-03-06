0fe5e9d4c3ef8ab1c568c3661622128fdbd1cb2a
Pedro D. Resende <pedro.damas-resende@esrf.fr>
2024-03-06 18:38:30 +0100

    finished uncertainty preparation

--- sagbo_analysis/dvc/dvc.py
+++ sagbo_analysis/dvc/dvc.py
@@ -44,10 +44,10 @@ class DVC_Setup:
     @property
     def meshing_dir(self):
         return os.path.join(self.processing_dir, "meshing")
-    
+
     @property
     def uncertainty_dir(self):
-        return os.path.join(self.dvc_dir, 'uncertainty')
+        return os.path.join(self.dvc_dir, "uncertainty")
 
     def build_folder_structure(self):
         try:
@@ -57,7 +57,7 @@ class DVC_Setup:
         try:
             os.mkdir(self.uncertainty_dir)
         except FileExistsError:
-            print('Uncertainty folder already exists, skipping.')
+            print("Uncertainty folder already exists, skipping.")
 
         self._link_vtks()
         self._link_images()
@@ -83,6 +83,7 @@ class DVC_Setup:
                 os.remove(dst)
                 os.symlink(src=tiff_name, dst=dst)
 
+
 class DVC_uncertainty(DVC_Setup):
 
     def __init__(self, config_file: str, increment: int = 1) -> None:
@@ -95,17 +96,16 @@ class DVC_uncertainty(DVC_Setup):
     @property
     def shifted_vol_path(self):
         return f"{os.path.splitext(self.ref_img_path)[0]}_shifted.tiff"
-        
 
     def _import_reference_volume(self):
-        vol = sk.io.imread(self.ref_img_path, plugin = 'tifffile')
+        vol = sk.io.imread(self.ref_img_path, plugin="tifffile")
         return vol
-    
+
     def _generate_random_shifts(self):
         return np.random.random(size=3)
-    
-    def _shift_volume(self, vol:np.ndarray, shifts:tuple):
-        
+
+    def _shift_volume(self, vol: np.ndarray, shifts: tuple):
+
         assert len(shifts) == vol.ndim
 
         dz, dy, dx = shifts
@@ -114,7 +114,7 @@ class DVC_uncertainty(DVC_Setup):
 
         return shifted_vol
 
-    def _write_shifts(self, shifts:tuple):
+    def _write_shifts(self, shifts: tuple):
 
         assert len(shifts) == 3
 
@@ -122,23 +122,29 @@ class DVC_uncertainty(DVC_Setup):
 
         d = datetime.now()
 
-        wrt_path = os.path.join(self.uncertainty_dir, f'shifts_{d.year}{d.month}{d.day}{d.hour}h{d.minute}.txt')
+        wrt_path = os.path.join(
+            self.uncertainty_dir,
+            f"shifts_{d.year}{d.month}{d.day}{d.hour}h{d.minute}.txt",
+        )
 
-        with open(wrt_path, 'w') as f:
-            f.write(f'coord, shift\n')
-            f.write(f'z, {dz}\n')
-            f.write(f'y, {dy}\n')
-            f.write(f'x, {dx}\n')
+        with open(wrt_path, "w") as f:
+            f.write(f"coord, shift\n")
+            f.write(f"z, {dz}\n")
+            f.write(f"y, {dy}\n")
+            f.write(f"x, {dx}\n")
 
-    def _save_shifted_vol(self, vol:np.ndarray):
-        
-        sk.io.imsave(self.shifted_vol_path, vol, plugin='tifffile')
+    def _save_shifted_vol(self, vol: np.ndarray):
 
+        sk.io.imsave(self.shifted_vol_path, vol, plugin="tifffile")
 
-    
+    def prepare_uncertainty_analysis(self):
 
+        vol = self._import_reference_volume()
 
+        shifts = self._generate_random_shifts()
 
+        self._write_shifts(shifts=shifts)
 
-        
-    
+        shifted_vol = self._shift_volume(vol=vol, shifts=shifts)
+
+        self._save_shifted_vol(vol=shifted_vol)
